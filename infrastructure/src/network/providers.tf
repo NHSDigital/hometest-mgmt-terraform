a46bc16f26ea5c582fe35075f94043f7159c8130
Mikolaj Miotk <mikolaj.miotk2@nhs.net>
2026-01-19 17:11:03 +0100

    Add dns queries real-time logging to S3

--- infrastructure/src/network/providers.tf
+++ infrastructure/src/network/providers.tf
@@ -18,3 +18,10 @@ provider "aws" {
   region              = var.aws_region
   allowed_account_ids = [var.aws_account_id]
 }
+
+# Provider for us-east-1 (required for Route 53 query logging CloudWatch logs)
+provider "aws" {
+  alias               = "us_east_1"
+  region              = "us-east-1"
+  allowed_account_ids = [var.aws_account_id]
+}
