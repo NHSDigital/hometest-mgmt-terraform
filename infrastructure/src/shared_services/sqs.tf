1a38b30ba224e2857937b8020458243a5093a7bb
piotrkas <88381351+piotrkas@users.noreply.github.com>
2026-02-12 10:39:11 +0100

    HOTE-545: TF module - AWS SQS (#23)
    * Initial SQS module
    
    * Refactor SQS module: simplify queue name logic and remove resource group configuration
    
    * Change name of the queue

---
+++ infrastructure/src/shared_services/sqs.tf
@@ -0,0 +1,121 @@
+################################################################################
+# SQS Queues
+# Shared queues for the hometest application
+################################################################################
+
+#------------------------------------------------------------------------------
+# Orders Queue
+# For processing test kit orders
+#------------------------------------------------------------------------------
+
+module "sqs_orders" {
+  source = "../../modules/sqs"
+
+  project_name      = var.project_name
+  environment       = var.environment
+  queue_name_suffix = "orders-results"
+
+  # Message configuration
+  visibility_timeout_seconds = var.orders_queue_visibility_timeout
+  message_retention_seconds  = var.orders_queue_retention_seconds
+  receive_wait_time_seconds  = 10 # Enable long polling
+
+  # DLQ configuration
+  create_dlq        = true
+  max_receive_count = var.orders_queue_max_receive_count
+
+  # Encryption
+  kms_master_key_id       = aws_kms_key.main.id
+  sqs_managed_sse_enabled = false
+
+  # CloudWatch alarms
+  create_cloudwatch_alarms = var.create_sqs_alarms
+  alarm_actions            = var.sqs_alarm_sns_topics
+  alarm_age_threshold      = var.orders_queue_age_threshold
+  alarm_depth_threshold    = var.orders_queue_depth_threshold
+
+  # Resource group
+  create_resource_group = true
+
+  tags = local.common_tags
+}
+
+#------------------------------------------------------------------------------
+# Notifications Queue (FIFO)
+# For reliable, ordered notification delivery
+#------------------------------------------------------------------------------
+
+module "sqs_notifications" {
+  source = "../../modules/sqs"
+
+  project_name      = var.project_name
+  environment       = var.environment
+  queue_name_suffix = "notifications"
+
+  # FIFO configuration
+  fifo_queue                  = true
+  content_based_deduplication = true
+  deduplication_scope         = "messageGroup"
+  fifo_throughput_limit       = "perMessageGroupId"
+
+  # Message configuration
+  visibility_timeout_seconds = var.notifications_queue_visibility_timeout
+  message_retention_seconds  = var.notifications_queue_retention_seconds
+  receive_wait_time_seconds  = 10
+
+  # DLQ configuration
+  create_dlq        = true
+  max_receive_count = var.notifications_queue_max_receive_count
+
+  # Encryption
+  kms_master_key_id       = aws_kms_key.main.id
+  sqs_managed_sse_enabled = false
+
+  # CloudWatch alarms
+  create_cloudwatch_alarms = var.create_sqs_alarms
+  alarm_actions            = var.sqs_alarm_sns_topics
+  alarm_age_threshold      = var.notifications_queue_age_threshold
+  alarm_depth_threshold    = var.notifications_queue_depth_threshold
+
+  # Resource group
+  create_resource_group = true
+
+  tags = local.common_tags
+}
+
+#------------------------------------------------------------------------------
+# Events Queue
+# For application events and audit logging
+#------------------------------------------------------------------------------
+
+module "sqs_events" {
+  source = "../../modules/sqs"
+
+  project_name      = var.project_name
+  environment       = var.environment
+  queue_name_suffix = "events"
+
+  # Message configuration
+  visibility_timeout_seconds = var.events_queue_visibility_timeout
+  message_retention_seconds  = var.events_queue_retention_seconds
+  receive_wait_time_seconds  = 10
+
+  # DLQ configuration
+  create_dlq        = true
+  max_receive_count = var.events_queue_max_receive_count
+
+  # Encryption
+  kms_master_key_id       = aws_kms_key.main.id
+  sqs_managed_sse_enabled = false
+
+  # CloudWatch alarms
+  create_cloudwatch_alarms = var.create_sqs_alarms
+  alarm_actions            = var.sqs_alarm_sns_topics
+  alarm_age_threshold      = var.events_queue_age_threshold
+  alarm_depth_threshold    = var.events_queue_depth_threshold
+
+  # Resource group
+  create_resource_group = true
+
+  tags = local.common_tags
+}
