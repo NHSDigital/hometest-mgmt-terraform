148f68830e45de257bcc0cf455d84613c7d230a3
Cormac Feeney <cormac.feeney1@nhs.net>
2026-03-05 12:01:11 +0000

    refactor: sqlfluff fix

--- infrastructure/src/lambda-goose-migrator/src/migrations/000009_update_supplier_endpoints.sql
+++ infrastructure/src/lambda-goose-migrator/src/migrations/000009_update_supplier_endpoints.sql
@@ -3,26 +3,30 @@ ALTER TABLE supplier
 ADD COLUMN results_path varchar(255);
 
 UPDATE supplier
-SET service_url = 'https://hometest-staging.prevx.io',
-    oauth_token_path = '/api/oauth/token',
-    results_path = '/api/results'
+SET
+  service_url = 'https://hometest-staging.prevx.io',
+  oauth_token_path = '/api/oauth/token',
+  results_path = '/api/results'
 WHERE supplier_id = '11111111-1111-4111-8111-111111111111';
 
 UPDATE supplier
-SET results_path = '/nhs_home_test/results',
-    order_path = '/nhs_home_test/order'
+SET
+  results_path = '/nhs_home_test/results',
+  order_path = '/nhs_home_test/order'
 WHERE supplier_id = '77777777-7777-4777-8777-777777777777';
 
 -- +goose Down
 UPDATE supplier
-SET service_url = 'https://func-nhshometest-staging.azurewebsites.net/',
-    oauth_token_path = '/api/oauth',
-    results_path = NULL
+SET
+  service_url = 'https://func-nhshometest-staging.azurewebsites.net/',
+  oauth_token_path = '/api/oauth',
+  results_path = NULL
 WHERE supplier_id = '11111111-1111-4111-8111-111111111111';
 
 UPDATE supplier
-SET order_path = '/order',
-    results_path = NULL
+SET
+  order_path = '/order',
+  results_path = NULL
 WHERE supplier_id = '77777777-7777-4777-8777-777777777777';
 
 ALTER TABLE supplier
