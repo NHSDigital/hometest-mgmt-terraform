7b9925bc553fd5a691cc456adfefa9a58e7e4fba
Cormac Feeney <cormac.feeney1@nhs.net>
2026-03-05 12:05:29 +0000

    refactor: add trailing / to service_url

--- infrastructure/src/lambda-goose-migrator/src/migrations/000009_update_supplier_endpoints.sql
+++ infrastructure/src/lambda-goose-migrator/src/migrations/000009_update_supplier_endpoints.sql
@@ -4,7 +4,7 @@ ADD COLUMN results_path varchar(255);
 
 UPDATE supplier
 SET
-  service_url = 'https://hometest-staging.prevx.io',
+  service_url = 'https://hometest-staging.prevx.io/',
   oauth_token_path = '/api/oauth/token',
   results_path = '/api/results'
 WHERE supplier_id = '11111111-1111-4111-8111-111111111111';
