46a857a350a81f6d4a3f6d0a59c6a826f7d8686a
CForde-Kainos <connor.forde1@nhs.net>
2026-03-05 15:21:40 +0000

    feature: update-order-status-codes

---
+++ infrastructure/src/lambda-goose-migrator/src/migrations/000010_update_order_status_codes.sql
@@ -0,0 +1,22 @@
+-- +goose Up
+UPDATE status_type
+SET status_code = 'SUBMITTED',
+    description = 'Order has been submitted to the supplier'
+WHERE status_code = 'PLACED';
+
+UPDATE status_type
+SET status_code = 'CONFIRMED',
+    description = 'Order has been confirmed by the supplier'
+WHERE status_code = 'ORDER_RECEIVED';
+
+-- +goose Down
+UPDATE status_type
+SET status_code = 'PLACED',
+description = 'Order has been placed with the supplier'
+WHERE status_code = 'SUBMITTED';
+
+UPDATE status_type
+SET status_code = 'ORDER_RECEIVED',
+    description = 'Order has been confirmed by the supplier'
+WHERE status_code = 'CONFIRMED';
+
