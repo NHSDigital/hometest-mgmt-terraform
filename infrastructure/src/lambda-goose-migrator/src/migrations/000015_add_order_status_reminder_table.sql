a03d358c8b8caaee4a6fe35a686b2bcd70188653
iichr <iichr@users.noreply.github.com>
2026-04-13 12:51:32 +0100

    fix: change delete to cascade

--- infrastructure/src/lambda-goose-migrator/src/migrations/000015_add_order_status_reminder_table.sql
+++ infrastructure/src/lambda-goose-migrator/src/migrations/000015_add_order_status_reminder_table.sql
@@ -2,45 +2,42 @@
 CREATE TABLE
 IF NOT EXISTS reminder_status_type
 (
-  status_code varchar
-(50) PRIMARY KEY,
+  status_code varchar(50) PRIMARY KEY,
   description text NOT NULL
 );
 
 INSERT INTO reminder_status_type
-  (status_code, description)
+(status_code, description)
 VALUES
-  ('SCHEDULED', 'Reminder scheduled to be sent, should be the initial status for a reminder'),
-  ('QUEUED', 'Reminder queued for sending'),
-  ('FAILED', 'Reminder failed to send'),
-  ('CANCELLED', 'Reminder cancelled')
+('SCHEDULED', 'Reminder scheduled to be sent, should be the initial status for a reminder'),
+('QUEUED', 'Reminder queued for sending'),
+('FAILED', 'Reminder failed to send'),
+('CANCELLED', 'Reminder cancelled')
 ON CONFLICT DO NOTHING;
 
 CREATE TABLE
 IF NOT EXISTS order_status_reminder
 (
-  reminder_id uuid PRIMARY KEY DEFAULT gen_random_uuid
-(),
+  reminder_id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
   order_uid uuid NOT NULL REFERENCES test_order
-(order_uid) ON
-DELETE RESTRICT,
+  (order_uid) ON
+  DELETE CASCADE,
   trigger_status varchar(50)
-NOT NULL REFERENCES status_type
-(status_code),
+  NOT NULL REFERENCES status_type
+  (status_code),
   reminder_number smallint NOT NULL CHECK
-(reminder_number >= 1),
-  status varchar
-(50) NOT NULL REFERENCES reminder_status_type
-(status_code),
+  (reminder_number >= 1),
+  status varchar(50) NOT NULL REFERENCES reminder_status_type
+  (status_code),
   triggered_at timestamp
-with time zone NOT NULL,
+  with time zone NOT NULL,
   sent_at timestamp
-with time zone,
+  with time zone,
   created_at timestamp
-with time zone NOT NULL DEFAULT current_timestamp,
+  with time zone NOT NULL DEFAULT current_timestamp,
   CONSTRAINT uq_order_status_reminder
   UNIQUE
-(order_uid, trigger_status, reminder_number)
+  (order_uid, trigger_status, reminder_number)
 );
 
 CREATE INDEX
